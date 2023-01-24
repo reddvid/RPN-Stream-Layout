2977cd9f675163de6c50c3c741fdccd866f1016d
David Ballesteros <david_ballesteros@outlook.com>
2023-01-24 16:53:02 +0800

    add forex data

--- RPNStreamControl.Wpf/MainWindow.xaml.cs
+++ RPNStreamControl.Wpf/MainWindow.xaml.cs
@@ -3,7 +3,10 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
+using System.Text.Json.Nodes;
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Timers;
 using System.Windows;
@@ -16,6 +19,11 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using System.Net.Http.Headers;
+using System.Net.Http;
+using System.Diagnostics;
+using Newtonsoft.Json;
+using System.Globalization;
 
 namespace RPNStreamControl.Wpf
 {
@@ -86,7 +94,7 @@ namespace RPNStreamControl.Wpf
 		private void ScrollTextBox_Loaded(object sender, RoutedEventArgs e)
 		{
 			string path = System.IO.Path.Combine(FILES_DIRECTORY, FILENAMES[3]);
-			
+
 			LoadText(path, tbxScroll);
 		}
 
@@ -134,5 +142,115 @@ namespace RPNStreamControl.Wpf
 
 			LoadText(path, tbxSubTitle);
 		}
+
+		private async void GetForex_Click(object sender, RoutedEventArgs e)
+		{
+			string url = "https://api.exchangerate.host/latest?base=PHP&symbols=USD,EUR,JPY,GBP,HKD,CAD,SGD,AUD,SAR,AED,KRW,CNY";
+
+			using HttpClient client = new();
+			client.DefaultRequestHeaders.Accept.Clear();
+
+			var json = await client.GetStringAsync(url);
+			var data = JsonConvert.DeserializeObject<Forex>(json);
+
+			StringBuilder sb = new();
+
+			if (data == null) return;
+
+			sb.Append("EXCHANGE RATES: 1 USD = ");
+			sb.Append((1 / data.Rates.USD).ToString("C2", CultureInfo.CurrentCulture) + " | 1 GBP = ");
+			sb.Append((1 / data.Rates.GBP).ToString("C2", CultureInfo.CurrentCulture) + " | 1 EUR = ");
+			sb.Append((1 / data.Rates.EUR).ToString("C2", CultureInfo.CurrentCulture) + " | 1 SAR = ");
+			sb.Append((1 / data.Rates.SAR).ToString("C2", CultureInfo.CurrentCulture) + " | 1 AED = ");
+			sb.Append((1 / data.Rates.AED).ToString("C2", CultureInfo.CurrentCulture) + " | 1 CAD = ");
+			sb.Append((1 / data.Rates.CAD).ToString("C2", CultureInfo.CurrentCulture) + " | 1 SGD = ");
+			sb.Append((1 / data.Rates.SGD).ToString("C2", CultureInfo.CurrentCulture) + " | 1 KRW = ");
+			sb.Append((1 / data.Rates.KRW).ToString("C2", CultureInfo.CurrentCulture) + " | 1 HKD = ");
+			sb.Append((1 / data.Rates.HKD).ToString("C2", CultureInfo.CurrentCulture) + " | 1 CNY = ");
+			sb.Append((1 / data.Rates.CNY).ToString("C2", CultureInfo.CurrentCulture) + " | 1 JPY = ");
+			sb.Append((1 / data.Rates.JPY).ToString("C2", CultureInfo.CurrentCulture) + " | 1 AUD = ");
+			sb.Append((1 / data.Rates.AUD).ToString("C2", CultureInfo.CurrentCulture) + " *** ");
+
+			tbxForex.Text = sb.ToString();
+		}
+
+		// 
+		public class Motd
+		{
+			[JsonProperty("msg")]
+			public string Msg { get; set; }
+
+			[JsonProperty("url")]
+			public string Url { get; set; }
+		}
+
+		public class Rates
+		{
+			public List<double> RateList { get; private set; }
+
+			public Rates()
+			{
+				RateList = new List<double>()
+				{
+					AED, AUD, CAD, CNY, EUR, GBP, HKD, JPY, KRW, SAR, SGD, USD
+				};
+			}
+
+			[JsonProperty("AED")]
+			public double AED { get; set; }
+
+			[JsonProperty("AUD")]
+			public double AUD { get; set; }
+
+			[JsonProperty("CAD")]
+			public double CAD { get; set; }
+
+			[JsonProperty("CNY")]
+			public double CNY { get; set; }
+
+			[JsonProperty("EUR")]
+			public double EUR { get; set; }
+
+			[JsonProperty("GBP")]
+			public double GBP { get; set; }
+
+			[JsonProperty("HKD")]
+			public double HKD { get; set; }
+
+			[JsonProperty("JPY")]
+			public double JPY { get; set; }
+
+			[JsonProperty("KRW")]
+			public double KRW { get; set; }
+
+			[JsonProperty("SAR")]
+			public double SAR { get; set; }
+
+			[JsonProperty("SGD")]
+			public double SGD { get; set; }
+
+			[JsonProperty("USD")]
+			public double USD { get; set; }
+		}
+
+		public class Forex
+		{
+			[JsonProperty("motd")]
+			public Motd Motd { get; set; }
+
+			[JsonProperty("success")]
+			public bool Success { get; set; }
+
+			[JsonProperty("base")]
+			public string Base { get; set; }
+
+			[JsonProperty("date")]
+			public string Date { get; set; }
+
+			[JsonProperty("rates")]
+			public Rates Rates { get; set; }
+		}
+
+
 	}
 }
