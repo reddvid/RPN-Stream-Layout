1774c3de2c5c0e4cc697c8832c80a8911346c772
David Ballesteros <david_ballesteros@outlook.com>
2023-05-29 18:38:40 +0800

    add speedtest to info

--- RPNStreamControl.Wpf/Views/DeviceInfoWindow.xaml.cs
+++ RPNStreamControl.Wpf/Views/DeviceInfoWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using Hardware.Info;
+using SpeedTest;
+using SpeedTest.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -45,11 +47,22 @@ namespace RPNStreamControl.Wpf.Views
 
 		private async Task RefreshDetailsAsync()
 		{
+			Title += " " + new System.Net.WebClient().DownloadString("https://api.ipify.org");
+
 			loading.IsActive = true;
 			loading.Visibility = Visibility.Visible;
 			loadingGrid.Visibility = Visibility.Visible;
 
-			var task = Task.Run(() => hardwareInfo.RefreshAll());
+			var task = Task.Run(() =>
+			{
+				hardwareInfo.RefreshCPUList();
+				hardwareInfo.RefreshMotherboardList();
+				hardwareInfo.RefreshMemoryList();
+				hardwareInfo.RefreshBIOSList();
+				hardwareInfo.RefreshDriveList();
+				hardwareInfo.RefreshOperatingSystem();
+				hardwareInfo.RefreshVideoControllerList();
+			});
 
 			await task.ContinueWith(t =>
 			{
@@ -57,15 +70,40 @@ namespace RPNStreamControl.Wpf.Views
 			{
 				new DeviceDetials("CPU", hardwareInfo.CpuList.FirstOrDefault()!.Name),
 				new DeviceDetials("Motherboard", hardwareInfo.MotherboardList.FirstOrDefault()!.Product),
-				new DeviceDetials("Motherboard", hardwareInfo.BiosList.FirstOrDefault()!.Version),
+				new DeviceDetials("BIOS", GetBios(hardwareInfo.BiosList)),
 				new DeviceDetials("Memory", GetMemory(hardwareInfo.MemoryList)),
 				new DeviceDetials("Storage", GetStorage(hardwareInfo.DriveList)),
 				new DeviceDetials("Video", GetGpu(hardwareInfo.VideoControllerList)),
 				new DeviceDetials("OS", $"{hardwareInfo.OperatingSystem.Name} {hardwareInfo.OperatingSystem.Version}"),
+				new DeviceDetials($"Speed test {DateTime.Now.ToString("f")}", GetSpeedTest())
 			};
 
 				ShowData(details);
-			}); 
+			});
+		}
+
+		private string GetSpeedTest()
+		{
+			var speedTestClient = new SpeedTestClient();
+			var settings = speedTestClient.GetSettings();
+			var server = settings.Servers[0];
+			var latency = speedTestClient.TestServerLatency(server);
+			var download = speedTestClient.TestDownloadSpeed(server);
+			var upload = speedTestClient.TestUploadSpeed(server);
+
+			return $"{server.Host} {server.Name}, {server.Country} {latency}ms {download / 1000, 0:N2}Mbps {upload / 1000, 0:N2}Mbps";
+		}
+
+		private string GetBios(List<BIOS> biosList)
+		{
+			string bios = string.Empty;
+
+			foreach (BIOS b in biosList)
+			{
+				bios += $"{b.Manufacturer} {b.Name} {b.Version} {b.SoftwareElementID}\n";
+			}
+
+			return bios.TrimEnd('\n');
 		}
 
 		private string GetGpu(List<VideoController> videoControllerList)
@@ -86,7 +124,7 @@ namespace RPNStreamControl.Wpf.Views
 
 			foreach (Drive dr in driveList)
 			{
-				drive += $"{dr.Model} {dr.Manufacturer} {dr.Size / Math.Pow(1024,3), 0:N0}GB\n";
+				drive += $"{dr.Model} {dr.Manufacturer} {dr.Size / Math.Pow(1024, 3),0:N0}GB\n";
 			}
 
 			return drive.TrimEnd('\n');
@@ -98,7 +136,7 @@ namespace RPNStreamControl.Wpf.Views
 
 			foreach (Memory mem in memoryList)
 			{
-				memory += $"{mem.Manufacturer} {mem.PartNumber.Trim()} {mem.Capacity / Math.Pow(1024,3)}GB {mem.Speed}MHz\n";
+				memory += $"{mem.Manufacturer} {mem.PartNumber.Trim()} {mem.Capacity / Math.Pow(1024, 3)}GB {mem.Speed}MHz\n";
 			}
 
 			return memory.TrimEnd('\n');
